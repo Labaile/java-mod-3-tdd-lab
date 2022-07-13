b240b4da73096ef4028e8fa183d891cc76a79b5e
Labaile <Labaile1@gmail.com>
2022-07-13 14:00:41 -0400

    Wrote a test to validate the "start with f" scenario and test fails

---
+++ src/test/java/FizzBuzzTest.java
@@ -0,0 +1,48 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class FizzBuzzTest {
+    FizzBuzz f;
+
+    @BeforeEach
+    void setUp() {
+        f = new FizzBuzz();
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void fizzBuzzString() {
+        String testString = "fub";
+        String str = f.fizzBuzzString(testString);
+        assertEquals("FizzBuzz", str);
+    }
+
+    @Test
+    void startswithf() {
+        String testString = "fun";
+        String str = f.fizzBuzz(testString);
+        assertEquals("Fizz", str);
+    }
+
+
+    @Test
+    void endwithb() {
+        String testString = "tub";
+        String str = f.fizzBuzzString(testString);
+        assertEquals("Buzz", str);
+    }
+
+    @Test
+    void returnsnull() {
+        String testString = null;
+        String str = f.fizzBuzz(testString);
+        assertEquals(null, str);
+    }
+
+}
